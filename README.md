3e275a3fc1706e177aa0e3e353d99de2e31c6986
Hearty, Oh <heartyoh@hatiolab.com>
2020-07-31 15:25:18 +0900

    upgrade dependencies to latest

---
+++ README.md
@@ -0,0 +1,35 @@
+[comment]: # "NOTE: This file is generated and should not be modify directly. Update `templates/ROOT_README.hbs.md` instead"
+
+# Things Factory Javascript SDK
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md)
+[![Build Status](https://travis-ci.org/things-factory-sdk/sdk.svg?branch=master)](https://travis-ci.org/things-factory-sdk/sdk)
+[![codecov](https://codecov.io/gh/things-factory-sdk/sdk/branch/master/graph/badge.svg)](https://codecov.io/gh/things-factory-sdk/sdk)
+[![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lernajs.io/)
+
+Javascript SDKs for ThingsFactory.
+
+These libraries compose together to help you create performant modern JS apps that you love to develop and test. These packages are developed primarily to be used on top of the stack we like best for our JS apps; Typescript for the flavor, Koa for the server, React for UI, Apollo for data fetching, and Jest for tests. That said, you can mix and match as you like.
+
+## Usage
+
+The sdk repo is managed as a monorepo that is composed of 1 npm packages.
+Each package has its own `README.md` and documentation describing usage.
+
+### Package Index
+
+| Package | Version | Description |
+| ------- | ------- | ----------- |
+
+
+| [koa-auth](packages/koa-auth) | <a href="https://badge.fury.io/js/%40things-factory-sdk%2Fkoa-auth"><img src="https://badge.fury.io/js/%40things-factory-sdk%2Fkoa-auth.svg" width="200px" /></a> | Middleware to authenticate a Koa application with ThingsFactory |
+
+## Want to contribute?
+
+Check out our [Contributing Guide](./.github/CONTRIBUTING.md)
+
+## License
+
+MIT &copy; [Hatiolab](https://www.hatiolab.com/), see [LICENSE.md](LICENSE.md) for details.
+
+<a href="http://www.hatiolab.com/"><img src="https://www.hatiolab.com/assets/img/logo.png" alt="Hatiolab" width="200" /></a>
--- plopfile.js
+++ plopfile.js
@@ -1,131 +1,119 @@
-const { readdirSync, existsSync } = require("fs");
-const path = require("path");
+const { readdirSync, existsSync } = require('fs')
+const path = require('path')
 
-const jsPackages = getPackages("js");
-const gems = getPackages("ruby");
+const jsPackages = getPackages('js')
+// const gems = getPackages("ruby");
 
 module.exports = function (plop) {
-  plop.setGenerator("package", {
-    description: "Create a new package from scratch",
+  plop.setGenerator('package', {
+    description: 'Create a new package from scratch',
     prompts: [
       {
-        type: "input",
-        name: "name",
+        type: 'input',
+        name: 'name',
         message: "What should this package's name be? Ex. react-utilities",
         validate: validatePackageName,
-        filter: plop.getHelper("kebabCase"),
+        filter: plop.getHelper('kebabCase')
       },
       {
-        type: "input",
-        name: "description",
+        type: 'input',
+        name: 'description',
         message: "What should this package's description be?",
-        filter: stripDescription,
+        filter: stripDescription
       },
       {
-        type: "confirm",
-        name: "usedInBrowser",
-        message: "Is this package intended for use in the browser?",
-      },
+        type: 'confirm',
+        name: 'usedInBrowser',
+        message: 'Is this package intended for use in the browser?'
+      }
     ],
     actions: [
       {
-        type: "add",
-        path: "packages/{{name}}/package.json",
-        templateFile: "templates/package.hbs.json",
+        type: 'add',
+        path: 'packages/{{name}}/package.json',
+        templateFile: 'templates/package.hbs.json'
       },
       {
-        type: "add",
-        path: "packages/{{name}}/tsconfig.json",
-        templateFile: "templates/tsconfig.hbs.json",
+        type: 'add',
+        path: 'packages/{{name}}/tsconfig.json',
+        templateFile: 'templates/tsconfig.hbs.json'
       },
       {
-        type: "add",
-        path: "packages/{{name}}/README.md",
-        templateFile: "templates/README.hbs.md",
+        type: 'add',
+        path: 'packages/{{name}}/README.md',
+        templateFile: 'templates/README.hbs.md'
       },
       {
-        type: "add",
-        path: "packages/{{name}}/CHANGELOG.md",
-        templateFile: "templates/CHANGELOG.hbs.md",
+        type: 'add',
+        path: 'packages/{{name}}/CHANGELOG.md',
+        templateFile: 'templates/CHANGELOG.hbs.md'
       },
       {
-        type: "add",
-        path: "packages/{{name}}/src/index.ts",
-        templateFile: "templates/index.hbs",
+        type: 'add',
+        path: 'packages/{{name}}/src/index.ts',
+        templateFile: 'templates/index.hbs'
       },
       {
-        type: "add",
-        path: "packages/{{name}}/src/{{properCase name}}.ts",
-        templateFile: "templates/my-package.hbs.ts",
+        type: 'add',
+        path: 'packages/{{name}}/src/{{properCase name}}.ts',
+        templateFile: 'templates/my-package.hbs.ts'
       },
       {
-        type: "add",
-        path: "packages/{{name}}/src/test/{{properCase name}}.test.ts",
-        templateFile: "templates/test.hbs.ts",
+        type: 'add',
+        path: 'packages/{{name}}/src/test/{{properCase name}}.test.ts',
+        templateFile: 'templates/test.hbs.ts'
       },
       {
-        type: "modify",
-        path: "packages/tsconfig.json",
+        type: 'modify',
+        path: 'packages/tsconfig.json',
         transform: (file, { name }) => {
-          const tsConfig = JSON.parse(file);
-          tsConfig.references.push({ path: `./${name}` });
-          tsConfig.references.sort(
-            ({ path: firstPath }, { path: secondPath }) =>
-              firstPath.localeCompare(secondPath)
-          );
-          return JSON.stringify(tsConfig);
-        },
-      },
-    ],
-  });
+          const tsConfig = JSON.parse(file)
+          tsConfig.references.push({ path: `./${name}` })
+          tsConfig.references.sort(({ path: firstPath }, { path: secondPath }) => firstPath.localeCompare(secondPath))
+          return JSON.stringify(tsConfig)
+        }
+      }
+    ]
+  })
 
-  plop.setGenerator("docs", {
-    description: "Generate root repo documentation",
+  plop.setGenerator('docs', {
+    description: 'Generate root repo documentation',
     prompts: [],
     actions: [
       {
-        type: "add",
-        path: "README.md",
-        templateFile: "templates/ROOT_README.hbs.md",
+        type: 'add',
+        path: 'README.md',
+        templateFile: 'templates/ROOT_README.hbs.md',
         force: true,
-        data: { jsPackages, gems },
-      },
-    ],
-  });
-};
+        data: { jsPackages /* gems */ }
+      }
+    ]
+  })
+}
 
-function getPackages(type = "js") {
-  const packagesPath = path.join(
-    __dirname,
-    type === "js" ? "packages" : "gems"
-  );
+function getPackages(type = 'js') {
+  const packagesPath = path.join(__dirname, type === 'js' ? 'packages' : 'gems')
 
   return readdirSync(packagesPath).reduce((acc, packageName) => {
-    const packageJSONPath = path.join(
-      packagesPath,
-      packageName,
-      "package.json"
-    );
+    const packageJSONPath = path.join(packagesPath, packageName, 'package.json')
 
     if (existsSync(packageJSONPath)) {
-      const { name, description } = require(packageJSONPath);
+      const { name, description } = require(packageJSONPath)
 
-      acc.push({ name: name.replace("@things-factory-sdk/", ""), description });
+      acc.push({ name: name.replace('@things-factory-sdk/', ''), description })
     }
 
-    return acc;
-  }, []);
+    return acc
+  }, [])
 }
 
 function validatePackageName(name) {
   return (
-    /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/g.test(
-      `@things-factory-sdk/${name}`
-    ) ||
+    /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/g.test(`@things-factory-sdk/${name}`) ||
     `Your package name (@things-factory-sdk/${name}) does not confirm to npm rules!`
-  );
+  )
 }
 
 function stripDescription(desc) {
-  return desc.replace(/[.\s]*$/g, "").replace(/^\s*/g, "");
+  return desc.replace(/[.\s]*$/g, '').replace(/^\s*/g, '')
 }
